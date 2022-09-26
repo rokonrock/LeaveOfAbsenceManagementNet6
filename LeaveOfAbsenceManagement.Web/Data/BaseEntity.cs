c384013847e753a49a5045d547f904f8a9436180
Abdullah Al Mamun <abdullah.almamun@inmarsat.com>
2022-09-26 16:11:15 -0230

    Added Database Tables and Migrations. Implement Database Section.

---
+++ LeaveOfAbsenceManagement.Web/Data/BaseEntity.cs
@@ -0,0 +1,9 @@
+﻿namespace LeaveOfAbsenceManagement.Web.Data
+{
+    public abstract class BaseEntity
+    {
+        public int Id { get; set; }
+        public DateTime DateCreated { get; set; }
+        public DateTime DateModified { get; set; }
+    }
+}
